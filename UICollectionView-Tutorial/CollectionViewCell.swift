1b0b775483d1a17d0b93e14a8096518ba1af74a9
damlacim <cim.damla@gmail.com>
2022-03-11 15:36:13 +0300

    create collection view controller

---
+++ UICollectionView-Tutorial/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  UICollectionView-Tutorial
+//
+//  Created by Damla Çim on 11.03.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
