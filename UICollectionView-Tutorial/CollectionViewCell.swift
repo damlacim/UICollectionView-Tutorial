ab44d37f34cbcbc7b2e3b05d724de2edb8e991ab
damlacim <cim.damla@gmail.com>
2022-03-11 17:13:25 +0300

    configuration cell

--- UICollectionView-Tutorial/CollectionViewCell.swift
+++ UICollectionView-Tutorial/CollectionViewCell.swift
@@ -9,4 +9,7 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var studentNameLabel: UILabel!
+    
+    
 }
--- UICollectionView-Tutorial/CollectionViewController.swift
+++ UICollectionView-Tutorial/CollectionViewController.swift
@@ -13,8 +13,44 @@ class CollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            
+            let rowElements : CGFloat = 2
+            let space: CGFloat = 5
+            let sumSpace = space * (rowElements - 1)
+            let itemSpace = sumSpace / rowElements
+            let width = collectionView.frame.width / rowElements - itemSpace
+            let height = width
+            
+            layout.itemSize = CGSize(width: width, height: height)
+            //itemlar arası boşluğu ayarlar
+            layout.minimumInteritemSpacing = space
+            //satırlar arası boşluğu ayarlar
+            layout.minimumLineSpacing = space
+        }
+    
 
         
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        if let customCell = cell as? CollectionViewCell {
+            customCell.studentNameLabel.text = "Student \(indexPath.row)"
+            return customCell
+        }
+        
+        return cell
     }
 
 }
